e144d258bb7c144dcdd2bab0f9fc1a39ccf0e65c
ggabriel360p <gabrielmark210@gmail.com>
2023-03-22 11:45:17 -0300

    veriff-email

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,9 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verify-email'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth','verify-email')->group(function () {
     // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
